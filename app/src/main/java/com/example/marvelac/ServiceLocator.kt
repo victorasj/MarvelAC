6d5999bc33e382a42621d83d8df4bc784b89ff1a
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-03-22 16:19:46 +0100

    Arreglo clase

--- app/src/main/java/com/example/marvelac/ServiceLocator.kt
+++ app/src/main/java/com/example/marvelac/ServiceLocator.kt
@@ -21,7 +21,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-k
+
 fun Application.initServiceLocator(){
     startKoin {
         androidLogger()
